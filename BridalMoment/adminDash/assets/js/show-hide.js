37a45bcfc3d458e922b379ec55c57d48b5bf10fa
Shahad Ishraq <shahad.nowhere@gmail.com>
2018-04-17 09:52:51 +0600

    Added some more pages

---
+++ BridalMoment/adminDash/assets/js/show-hide.js
@@ -0,0 +1,44 @@
+function test1(a) {
+
+	if(a.checked){
+		
+		$("#row1").show();
+		
+	}
+	else{
+		
+		$("#row1").hide();
+		
+	}
+
+}
+
+function test2(a) {
+
+	if(a.checked){
+		
+		$("#row2").show();
+		
+	}
+	else{
+		
+		$("#row2").hide();
+		
+	}
+
+}
+
+function test3(a) {
+
+	if(a.checked){
+		
+		$("#row3").show();
+		
+	}
+	else{
+		
+		$("#row3").hide();
+		
+	}
+
+}
